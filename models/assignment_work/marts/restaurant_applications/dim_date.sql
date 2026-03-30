d0a126f51a6ed46fef2431d8e16cad8ea4cfb55d
Mariana Lima <maribflima@gmail.com>
2026-03-30 20:43:02 +0000

    updated sql

--- models/assignment_work/marts/restaurant_applications/dim_date.sql
+++ models/assignment_work/marts/restaurant_applications/dim_date.sql
@@ -1,18 +1,20 @@
-WITH all_dates AS (
+-- Date dimension shared by both restaurant applications and 311 requests
 
+WITH all_dates AS (
+   -- Get dates (dates, no time included) from 311 requests
    SELECT DISTINCT CAST(created_date AS DATE) AS full_date
-   FROM {{ ref('stg_dot_service_requests') }}
+   FROM {{ ref('stg_nyc_311_dot') }}
    WHERE created_date IS NOT NULL
 
    UNION DISTINCT
 
+   -- Get dates from restaurant applications
    SELECT DISTINCT CAST(time_of_submission AS DATE) AS full_date
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE time_of_submission IS NOT NULL
 ),
 
 date_dimension AS (
-
    SELECT
        {{ dbt_utils.generate_surrogate_key(['full_date']) }} AS date_key,
 
@@ -24,11 +26,7 @@ date_dimension AS (
        EXTRACT(DAY FROM full_date) AS day_of_month,
        EXTRACT(DAYOFWEEK FROM full_date) AS day_of_week,
        FORMAT_DATE('%A', full_date) AS day_name,
-
-       CASE 
-           WHEN EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) THEN TRUE
-           ELSE FALSE
-       END AS is_weekend,
+       EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
 
        CASE
            WHEN EXTRACT(MONTH FROM full_date) >= 7 THEN EXTRACT(YEAR FROM full_date) + 1
