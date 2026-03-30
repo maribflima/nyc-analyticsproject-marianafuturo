d52901c48c89f174aba677bb98433c1566e7f807
Mariana Lima <maribflima@gmail.com>
2026-03-30 19:09:20 +0000

    updated sql code

--- models/assignment_work/marts/dimensions/dim_date.sql
+++ models/assignment_work/marts/dimensions/dim_date.sql
@@ -1,25 +1,14 @@
 -- Date dimension shared by both restaurant applications and 311 requests
 
 WITH all_dates AS (
-   -- Get dates (dates, no time included) from 311 requests
-   SELECT DISTINCT CAST(created_date AS DATE) AS full_date
-   FROM {{ ref('stg_nyc_311_dot') }}
-   WHERE created_date IS NOT NULL
 
-   UNION DISTINCT
-
-   -- Get dates from restaurant applications
-   SELECT-- Date dimension shared by both restaurant applications and 311 requests
-
-WITH all_dates AS (
-
-    SELECT DISTINCT CAST(created_date AS DATE) AS full_date
+    SELECT DISTINCT DATE(created_date) AS full_date
     FROM {{ ref('stg_nyc_311_dot') }}
     WHERE created_date IS NOT NULL
 
-    UNION
+    UNION DISTINCT
 
-    SELECT DISTINCT CAST(time_of_submission AS DATE) AS full_date
+    SELECT DISTINCT DATE(time_of_submission) AS full_date
     FROM {{ ref('stg_nyc_open_restaurant_apps') }}
     WHERE time_of_submission IS NOT NULL
 
@@ -28,7 +17,7 @@ WITH all_dates AS (
 date_dimension AS (
 
     SELECT
-        TO_HEX(MD5(CAST(full_date AS STRING))) AS date_key,
+        FORMAT('%t', full_date) AS date_key,  -- simple stable key (BigQuery-safe)
 
         full_date,
         EXTRACT(YEAR FROM full_date) AS year,
@@ -50,31 +39,4 @@ date_dimension AS (
 )
 
 SELECT *
-FROM date_dimension DISTINCT CAST(time_of_submission AS DATE) AS full_date
-   FROM {{ ref('stg_nyc_open_restaurant_apps') }}
-   WHERE time_of_submission IS NOT NULL
-),
-
-date_dimension AS (
-   SELECT
-       {{ dbt_utils.generate_surrogate_key(['full_date']) }} AS date_key,
-
-       full_date,
-       EXTRACT(YEAR FROM full_date) AS year,
-       EXTRACT(QUARTER FROM full_date) AS quarter,
-       EXTRACT(MONTH FROM full_date) AS month,
-       FORMAT_DATE('%B', full_date) AS month_name,
-       EXTRACT(DAY FROM full_date) AS day_of_month,
-       EXTRACT(DAYOFWEEK FROM full_date) AS day_of_week,
-       FORMAT_DATE('%A', full_date) AS day_name,
-       EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
-
-       CASE
-           WHEN EXTRACT(MONTH FROM full_date) >= 7 THEN EXTRACT(YEAR FROM full_date) + 1
-           ELSE EXTRACT(YEAR FROM full_date)
-       END AS fiscal_year
-
-   FROM all_dates
-)
-
-SELECT * FROM date_dimension
+FROM date_dimension 
