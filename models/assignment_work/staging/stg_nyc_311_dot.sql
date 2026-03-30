4ae5cfb3f8bc54872ba6334c6aa426364a7e0f79
Mariana Lima <maribflima@gmail.com>
2026-03-30 19:31:19 +0000

    updated sql

--- models/assignment_work/staging/stg_nyc_311_dot.sql
+++ models/assignment_work/staging/stg_nyc_311_dot.sql
@@ -2,7 +2,7 @@
 -- One row per service request
 
 WITH source AS (
-   SELECT * FROM {{ source('raw', 'base_dot_311') }}
+   SELECT * FROM {{ source('raw', 'source_dot_service_requests_history') }}
 ), -- Easier to refer to the dbt reference to a long name table this way
 
 cleaned AS (
