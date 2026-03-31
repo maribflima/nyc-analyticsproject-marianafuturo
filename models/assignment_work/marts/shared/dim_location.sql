53b4b31f8248045f2ea2011f39d545bd73fbd1e3
Mariana Lima <maribflima@gmail.com>
2026-03-31 14:58:07 +0000

    updated dim location

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -3,7 +3,7 @@ WITH all_locations AS (
    -- Get locations from 311 requests
    SELECT DISTINCT
       borough,
-      zip_code
+      incident_zip AS zip_code
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
 
@@ -12,7 +12,7 @@ WITH all_locations AS (
    -- Get locations from restaurant applications
    SELECT DISTINCT
       borough,
-      zip_code
+      zip AS zip_code
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
 ),
